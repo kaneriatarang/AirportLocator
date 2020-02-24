6d6106d02ed9711621745de1bf4446512961eb93
Tarang Kaneriya <tarangk@crimsonlogic.com>
2020-02-24 16:28:17 +0530

    Small Changes

---
+++ Pods/Target Support Files/Pods-AirAsiaAirportLocator-AirAsiaAirportLocatorUITests/Pods-AirAsiaAirportLocator-AirAsiaAirportLocatorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AirAsiaAirportLocator_AirAsiaAirportLocatorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AirAsiaAirportLocator_AirAsiaAirportLocatorUITests
+@end
---
+++ Pods/Target Support Files/Pods-AirAsiaAirportLocator/Pods-AirAsiaAirportLocator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AirAsiaAirportLocator : NSObject
+@end
+@implementation PodsDummy_Pods_AirAsiaAirportLocator
+@end
